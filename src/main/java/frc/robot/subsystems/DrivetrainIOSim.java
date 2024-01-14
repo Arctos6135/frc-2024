ed33a4d0655bc1fc3de45f88c326bd8d122d072a
n-arms <75282394+n-arms@users.noreply.github.com>
2024-01-13 22:09:05 -0500

    Add drivetrain simulation

---
+++ src/main/java/frc/robot/subsystems/DrivetrainIOSim.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotGearing;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotMotor;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim.KitbotWheelSize;
+
+public class DrivetrainIOSim extends DrivetrainIO {
+    private final DifferentialDrivetrainSim drive = DifferentialDrivetrainSim.createKitbotSim(
+        KitbotMotor.kDoubleNEOPerSide, KitbotGearing.k8p45, KitbotWheelSize.kSixInch, null
+    );
+
+    private double previousHeading = 0;
+
+    @Override
+    public void updateInputs(Inputs inputs) {
+        drive.update(0.02);
+
+        inputs.leftPosition = drive.getLeftPositionMeters();
+        inputs.rightPosition = drive.getRightPositionMeters();
+
+        inputs.leftVelocity = drive.getLeftVelocityMetersPerSecond();
+        inputs.rightVelocity = drive.getRightVelocityMetersPerSecond();
+
+        inputs.yaw = drive.getHeading().getRadians();
+        inputs.yawRate = (inputs.yaw - previousHeading) / 0.02;
+        previousHeading = inputs.yaw;
+    }
+}

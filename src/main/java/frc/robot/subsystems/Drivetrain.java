146d431cf12a4eea26255430cdf0ab199bc27aed
n-arms <75282394+n-arms@users.noreply.github.com>
2024-01-13 21:23:58 -0500

    Switch to using AdvantageKit drivetrain

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,59 +1,15 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.CANSparkBase.IdleMode;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-import frc.robot.constants.DriveConstants;
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.constants.CANBus;
 
 public class Drivetrain extends SubsystemBase {
-    private final CANSparkMax rightMaster = new CANSparkMax(CANBus.RIGHT_MASTER, MotorType.kBrushless);
-    private final CANSparkMax leftMaster = new CANSparkMax(CANBus.LEFT_MASTER, MotorType.kBrushless);
-    private final CANSparkMax rightFollower = new CANSparkMax(CANBus.RIGHT_FOLLOWER, MotorType.kBrushless);
-    private final CANSparkMax leftFollower = new CANSparkMax(CANBus.LEFT_FOLLOWER, MotorType.kBrushless);
-    private final RelativeEncoder rightEncoder;
-    private final RelativeEncoder leftEncoder;
+    private final DrivetrainIO io;
 
-    //Controllers
-    private final PIDController translationalController;
-    private final PIDController rotationController;
-
-    // Translational
-    private static double kP = 0.00025;
-    private static double kI = 0.000001;
-    private static double kD = 0.0150;
-
-    // Rotational
-    private static double rotP = 0.00001;
-    private static double rotI = 0.0;
-    private static double rotD = 0.0;
-
-    
-    public Drivetrain() {
-        this.rightFollower.follow(this.rightMaster);
-        this.leftFollower.follow(this.leftMaster);
-
-        this.rightMaster.setInverted(true);
-
-        this.leftMaster.setIdleMode(IdleMode.kBrake);
-        this.rightMaster.setIdleMode(IdleMode.kBrake);
-
-        this.translationalController = new PIDController(kP, kI, kD);
-        this.rotationController = new PIDController(rotP, rotI, rotD);
-
-        this.rightEncoder = this.rightMaster.getEncoder();
-        this.leftEncoder = this.leftMaster.getEncoder();
-
-        this.rightEncoder.setPositionConversionFactor(DriveConstants.POSITION_CONVERSION_FACTOR);
-        this.leftEncoder.setPositionConversionFactor(DriveConstants.POSITION_CONVERSION_FACTOR);
+    public Drivetrain(DrivetrainIO io) {
+        this.io = io;
     }
 
     public void arcadeDrive(double translation, double rotation) {
-        leftMaster.set(translation + rotation);
-        rightMaster.set(translation - rotation);
+        io.setVoltages(12 * (translation + rotation), 12 * (translation - rotation));
     }
 }
-
---
+++ src/main/java/frc/robot/subsystems/DrivetrainIO.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems;
+
+public class DrivetrainIO {
+    public void setVoltages(double left, double right) {}
+}

d91081cdcc91715348d06fb6e72ad8445f31947f
Ayaan Adrito <ayaan.adrito@gmail.com>
2024-01-21 18:17:30 -0500

    Finish RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,21 +5,35 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.commands.driving.TeleopDrive;
 import frc.robot.constants.DriveConstants;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.DrivetrainIO;
+import frc.robot.subsystems.DrivetrainIOSim;
+import frc.robot.subsystems.DrivetrainIOSparkMax;
+import frc.robot.constants.Controllers;
 
 public class RobotContainer {
   private final XboxController driverController = new XboxController(DriveConstants.DRIVER_CONTROLLER);
   private final XboxController operatorController = new XboxController(DriveConstants.OPERATOR_CONTROLLER);
   public final Drivetrain drivetrain;
-  public final DrivetrainIO drivetrainIO = new DrivetrainIO();
 
   public RobotContainer() {
-    this.drivetrain = new Drivetrain(drivetrainIO);
     configureBindings();
+
+    if (RobotBase.isReal()) {
+      this.drivetrain = new Drivetrain(new DrivetrainIOSparkMax());
+    } else if (RobotBase.isSimulation()) {
+      this.drivetrain = new Drivetrain(new DrivetrainIOSim());
+    } else {
+      this.drivetrain = new Drivetrain(new DrivetrainIO());
+    }
+
+    drivetrain.setDefaultCommand(new TeleopDrive(
+      drivetrain, driverController, Controllers.DRIVE_FWD_REV, Controllers.DRIVE_LEFT_RIGHT));
   }
 
   private void configureBindings() {}

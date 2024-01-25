b286f56dcc44c0a898188172581b325a64af720f
n-arms <75282394+n-arms@users.noreply.github.com>
2024-01-24 22:05:04 -0500

    Fix CANBus.java

---
+++ src/main/java/frc/robot/constants/CANBus.java
@@ -0,0 +1,11 @@
+package frc.robot.constants;
+
+public class CANBus {
+    public static final int DRIVE_RIGHT_MASTER = 1;
+    public static final int DRIVE_LEFT_MASTER = 4;
+    public static final int DRIVE_RIGHT_FOLLOWER = 3;
+    public static final int DRIVE_LEFT_FOLLOWER = 2;
+
+    public static final int INTAKE_TOP_MASTER = 5;
+    public static final int INTAKE_BOTTOM_MASTER = 6;
+}

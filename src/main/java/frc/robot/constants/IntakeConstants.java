70312d843855c7f10a2d0a424ec0311be60ac2dd
Ben M <benjt.mcmillan@icloud.com>
2024-01-24 21:48:52 -0500

    Created intake piece command

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -2,4 +2,8 @@ package frc.robot.constants;
 
 public class IntakeConstants {
     public static int MEDIAN_FILTER_SIZE = 14;
+
+    public static double TOP_VOLTAGE = 14;
+    public static double BOTTOM_VOLTAGE = 14;
+    public static double MAX_CURRENT = 14;
 }

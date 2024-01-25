89beb6c9db39870d9cffbfde99603cdaa5dd23a4
Ben M <benjt.mcmillan@icloud.com>
2024-01-24 21:23:07 -0500

    Added median filter to detect current spikes

---
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -0,0 +1,5 @@
+package frc.robot.constants;
+
+public class IntakeConstants {
+    public static int MEDIAN_FILTER_SIZE = 14;
+}
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,13 +1,18 @@
 package frc.robot.subsystems;
 import org.littletonrobotics.junction.Logger;
 
+import edu.wpi.first.math.filter.MedianFilter;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.IntakeConstants;
 
 
 public class Intake extends SubsystemBase {
     private final IntakeIO io;
 
     private final IntakeInputsAutoLogged inputs = new IntakeInputsAutoLogged();
+
+    private final MedianFilter filter = new MedianFilter(IntakeConstants.MEDIAN_FILTER_SIZE);
+    private double medianCurrent;
 
     public Intake(IntakeIO io) {
         this.io = io;
@@ -19,6 +24,8 @@ public class Intake extends SubsystemBase {
         io.updateInputs(inputs);
         // Log all the sensor data.
         Logger.processInputs("Intake", inputs);
+
+        medianCurrent = filter.calculate(inputs.current);
     }
 
     public double getPosition() {
@@ -27,6 +34,10 @@ public class Intake extends SubsystemBase {
 
     public void setVoltage(int voltage) {
         io.setVoltage(voltage);
+    }
+
+    public double getFilteredCurrent() {
+        return medianCurrent;
     }
 
 }

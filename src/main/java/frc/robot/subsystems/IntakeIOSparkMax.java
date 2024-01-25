573892ac20c2dd00221d4ceea38aba96931fc0e6
n-arms <75282394+n-arms@users.noreply.github.com>
2024-01-24 21:48:35 -0500

    Fix merge conflict bugs

--- src/main/java/frc/robot/subsystems/IntakeIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/IntakeIOSparkMax.java
@@ -3,14 +3,11 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.ADIS16470_IMU;
-
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 
 import frc.robot.constants.CANBus;
 import frc.robot.constants.IntakeConstants;
-import frc.robot.subsystems.IntakeIO.IntakeInputs;
 
 public class IntakeIOSparkMax extends IntakeIO{
     private final CANSparkMax top = new CANSparkMax(CANBus.INTAKE_TOP_MASTER, MotorType.kBrushless);

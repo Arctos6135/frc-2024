48116f8b5161cad730cefa5536346dc6a856180a
Raiyan <developer.rkbyte@gmail.com>
2024-02-03 13:00:41 -0500

    Final fix for speaker scoring (hopefully)

--- src/main/java/frc/robot/commands/scoring/Score.java
+++ src/main/java/frc/robot/commands/scoring/Score.java
@@ -1,7 +1,9 @@
 package frc.robot.commands.scoring;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.constants.ArmConstants;
+import frc.robot.constants.IntakeConstants;
 import frc.robot.constants.ShooterConstants;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.intake.Intake;
@@ -13,7 +15,7 @@ import frc.robot.commands.shooter.Launch;
 
 public class Score {
     public static Command scoreSpeaker(Arm arm, Shooter shooter, Intake intake) {        
-        return new ArmPID(arm, ArmConstants.SPEAKER_SCORING_POSITION).until(() -> checkPosition(arm, ArmConstants.SPEAKER_SCORING_POSITION)).andThen(new Launch(shooter, ShooterConstants.AMP_RPS)).andThen(new Feed(intake));
+        return new ArmPID(arm, ArmConstants.SPEAKER_SCORING_POSITION).raceWith(new WaitUntilCommand(() -> checkPosition(arm, ArmConstants.SPEAKER_SCORING_POSITION)).andThen(new Feed(intake)).andThen(new Launch(shooter, ShooterConstants.SPEAKER_RPS)));
     }
 
     public static boolean checkPosition(Arm arm, double targetPosition) {

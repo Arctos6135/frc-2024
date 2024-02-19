ce5585039be38598b9ffeaf2c22982579f13e16c
n-arms <75282394+n-arms@users.noreply.github.com>
2024-02-19 09:13:10 -0500

    Add velocity characterization routine

---
+++ src/main/java/frc/robot/commands/characterization/VelocityRoutine.java
@@ -0,0 +1,45 @@
+package frc.robot.commands.characterization;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class VelocityRoutine extends Command {
+    private final LoggedMechanismGroup mechanism;
+    private final double maxDistance;
+    private final double rampRate;
+    private double voltage = 0;
+    private double lastTimestamp;
+
+    public VelocityRoutine(LoggedMechanismGroup mechanism, double maxDistance, double rampRate) {
+        this.mechanism = mechanism;
+        this.maxDistance = maxDistance;
+        this.rampRate = rampRate;
+    }
+
+    @Override
+    public void initialize() {
+        lastTimestamp = Timer.getFPGATimestamp();
+    }
+
+    @Override
+    public void execute() {
+        double timestamp = Timer.getFPGATimestamp();
+        voltage += rampRate * (timestamp - lastTimestamp);
+        lastTimestamp = timestamp;
+
+        mechanism.setVoltage(voltage);
+    }
+
+    @Override
+    public boolean isFinished() {
+        boolean tooFar = mechanism.getGreatestDistance() > maxDistance;
+        boolean tooFarBack = mechanism.getLeastDistance() < 0;
+
+        return tooFar || tooFarBack;
+    }
+
+    @Override
+    public void end(boolean interupted) {
+        mechanism.setVoltage(0);
+    }
+}

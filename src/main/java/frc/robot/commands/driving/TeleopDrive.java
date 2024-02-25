723404c6cd52c08091832500bbd5b24254748373
Micah Hewko <micah.hewko@student.tdsb.on.ca>
2024-02-24 19:18:15 -0500

    Added deadzones to the driver controller so the robot wouldn't move around due to drift.

--- src/main/java/frc/robot/commands/driving/TeleopDrive.java
+++ src/main/java/frc/robot/commands/driving/TeleopDrive.java
@@ -2,6 +2,7 @@ package frc.robot.commands.driving;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.constants.ControllerConstants;
 import frc.robot.constants.DriveConstants;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.util.Dampener;
@@ -26,8 +27,12 @@ public class TeleopDrive extends Command {
     @Override
     public void execute() {
         double y = controller.getRawAxis(XboxController.Axis.kLeftY.value);
+        // Rounds y to zero if it's less than the controller's deadband.
+        if (Math.abs(y) < ControllerConstants.CONTROLLER_DEADZONE) {y = 0.0;}
         double y1 = -yDampener.dampen(y) * (precisionDrive ? DriveConstants.PRECISION_FWD_REV : 1.0);
         double x = controller.getRawAxis(XboxController.Axis.kRightX.value);
+        // Rounds x to zero if it's less than the controller's deadband.
+        if (Math.abs(x) < ControllerConstants.CONTROLLER_DEADZONE) {x = 0.0;}
         double x1 = xDampener.dampen(x) * (precisionDrive ? DriveConstants.PRECISION_TURN : 1.0);
 
         y1 *= DriveConstants.MAX_TELEOP_SPEED;

6b7e422563dd39fed17ae247c6ad44ad1e6bbb28
n-arms <75282394+n-arms@users.noreply.github.com>
2024-03-17 21:32:33 -0400

    Add a pull to update astrolabe

--- installAstrolabe.bat
+++ installAstrolabe.bat
@@ -3,6 +3,7 @@ git submodule update --init --recursive
 cd astrolabe
 echo FIXING GIT BRANCH
 git checkout remotes/origin/HEAD && git checkout master
+git pull
 echo BUILDING ASTROLABE
 ./gradlew build
 cd ..

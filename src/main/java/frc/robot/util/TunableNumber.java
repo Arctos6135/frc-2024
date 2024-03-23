26da568fb57932448bd62c8ced61b39a9be41cf7
n-arms <75282394+n-arms@users.noreply.github.com>
2024-03-22 22:21:07 -0400

    Got PID values logging to elastic

--- src/main/java/frc/robot/util/TunableNumber.java
+++ src/main/java/frc/robot/util/TunableNumber.java
@@ -2,6 +2,7 @@ package frc.robot.util;
 
 import edu.wpi.first.networktables.DoubleEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class TunableNumber {
     private static final String TABLE_ROOT = "TunableNumbers";
@@ -9,13 +10,21 @@ public class TunableNumber {
     private final DoubleEntry entry;
     private double lastEntry;
 
+    private final String name;
+    private final double defaultValue;
+
     public TunableNumber(String name, double defaultValue) {
         entry = NetworkTableInstance.getDefault().getTable(TABLE_ROOT).getDoubleTopic(name).getEntry(defaultValue);
         lastEntry = defaultValue;
+
+        this.name = name;
+        this.defaultValue = defaultValue;
+
+        SmartDashboard.putNumber(name, defaultValue);
     }
 
     public double get() {
-        return entry.get();
+        return SmartDashboard.getNumber(name, defaultValue);
     }
 
     public static void ifChanged(Runnable onChange, TunableNumber... watching) {
